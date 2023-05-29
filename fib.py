18f0fd5c3de7e64ef0afc81ddf1e19aa66224cba
franklin <ezewusifranklin@gmail.com>
2023-05-29 13:27:00 +0100

    fib num

--- fib.py
+++ fib.py
@@ -1,4 +1,11 @@
-def fib():
-    pass
+def fib(x):
+    fibonnaci_number = [0,1]  
+    for i in range(2, x):
+        fibonnaci_number.append(fibonnaci_number[i - 1] + fibonnaci_number[i - 2])
+    return fibonnaci_number
 
+
+fib_ten_numbers = fib(10)
+for numbers in fib_ten_numbers:
+    print(numbers, end=", ")
 
