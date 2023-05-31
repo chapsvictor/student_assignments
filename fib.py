e9f34d744a3ee1b16f34c09b17b1970f652e829b
collinsjie <collinagba5@gmail.com>
2023-05-31 15:36:19 +0100

    fib

--- fib.py
+++ fib.py
@@ -1,4 +1,4 @@
-
+'''I used function to write the first 10 range numbers of fibonacci'''
 def fibonacci(n):
     fib = [0, 1]  # Initialize the Fibonacci sequence with the first two numbers
     for i in range(2, n):
