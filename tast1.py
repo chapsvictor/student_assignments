52e64e8c639aeecd50a5616089c62caeac84c97f
collinsjie <collinagba5@gmail.com>
2023-06-18 16:26:34 +0100

    tast

--- tast1.py
+++ tast1.py
@@ -1,3 +1,4 @@
+""" wrote a code that print out age above 20 in a dictionary of some persons using function"""
 def print_ages_above_20(dictionary):
     for name, age in dictionary.items():
         if age > 20:
